96f8440bf8ca5df74ec0d07fd64acb97431924da
Alexis Palma <palmaalexis017@gmail.com>
2026-08-26 17:53:04 -0600

    feat: Add sqlite config

---
+++ src/infrastructure/persistence/sqlite/mappers/label.mapper.ts
@@ -0,0 +1,26 @@
+import { LabelColor } from '../../../../domain/aggregates/label/color.vo';
+import { LabelId } from '../../../../domain/aggregates/label/id.vo';
+import { Label } from '../../../../domain/aggregates/label/label.aggregate';
+import { LabelName } from '../../../../domain/aggregates/label/name.vo';
+import type { SQLiteLabelRecord } from '../records/label.record';
+
+export class SQLiteLabelMapper {
+  static toDomain(persistence: SQLiteLabelRecord): Label {
+    return Label.rehydrate(
+      LabelId.rehydrate(persistence.id),
+      LabelName.rehydrate(persistence.name),
+      LabelColor.rehydrate(persistence.color),
+      persistence.deletedAt ? new Date(persistence.deletedAt) : null
+    );
+  }
+
+  static toPersistence(domain: Label): SQLiteLabelRecord {
+    const primitives = domain.toPrimitives();
+    return {
+      id: primitives.id,
+      name: primitives.name,
+      color: primitives.color,
+      deletedAt: primitives.deletedAt?.toISOString() ?? null,
+    };
+  }
+}
---
+++ src/infrastructure/persistence/sqlite/mappers/task.mapper.ts
@@ -0,0 +1,43 @@
+import type { LabelId } from '../../../../domain/aggregates/label/id.vo';
+import type { SQLiteTaskRecord } from '../records/task.record';
+import { ProjectId } from '../../../../domain/aggregates/project/id.vo';
+import { TaskDescription } from '../../../../domain/aggregates/task/description.vo';
+import { TaskId } from '../../../../domain/aggregates/task/id.vo';
+import { TaskPriority } from '../../../../domain/aggregates/task/priority.vo';
+import { TaskStatus } from '../../../../domain/aggregates/task/status.vo';
+import { Task } from '../../../../domain/aggregates/task/task.aggregate';
+import { TaskTitle } from '../../../../domain/aggregates/task/title.vo';
+
+export class SQLiteTaskMapper {
+  static toDomain(persistence: SQLiteTaskRecord, labelId: LabelId[]): Task {
+    return Task.rehydrate(
+      TaskId.rehydrate(persistence.id),
+      ProjectId.rehydrate(persistence.projectId),
+      TaskTitle.rehydrate(persistence.title),
+      TaskStatus.rehydrate(persistence.status),
+      TaskPriority.rehydrate(persistence.priority),
+      new Date(persistence.createdAt),
+      persistence.startedAt ? new Date(persistence.startedAt) : null,
+      persistence.finishedAt ? new Date(persistence.finishedAt) : null,
+      persistence.deletedAt ? new Date(persistence.deletedAt) : null,
+      labelId,
+      persistence.description ? TaskDescription.rehydrate(persistence.description) : null
+    );
+  }
+
+  static toPersistence(domain: Task): SQLiteTaskRecord {
+    const primitives = domain.toPrimitives();
+    return {
+      id: primitives.id,
+      projectId: primitives.projectId,
+      title: primitives.title,
+      status: primitives.status,
+      priority: primitives.priority,
+      createdAt: primitives.createdAt.toISOString(),
+      startedAt: primitives.startedAt?.toISOString() ?? null,
+      finishedAt: primitives.finishedAt?.toISOString() ?? null,
+      deletedAt: primitives.deletedAt?.toISOString() ?? null,
+      description: primitives.description,
+    };
+  }
+}
